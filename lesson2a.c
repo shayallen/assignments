605ecb3edecd5852b08daf78c870987f94ee1780
Shay Allen <shayallen@gmail.com>
2013-02-28 08:34:55 -0700

    Trying to fix bugs to test, but having no luck

--- lesson2a.c
+++ lesson2a.c
@@ -4,31 +4,33 @@
 int main(void){
 	int value1,value2,answer,assn_operator,i;
 	char operator1;
+
 	//variable asignments
-	i = 0
+	i = 0;
 	srand(time(NULL));	
 	value1 = rand() % 999;
 	value2 = rand() % 999;
 	assn_operator = rand() % 3;
 	if(assn_operator == 0){
-		operator1=+;
+		operator1='+';
 	}else if(assn_operator == 1){
-		operator1=-;
+		operator1='-';
 	}else if(assn_operator == 2){
-		operator1=/;
+		operator1='/';
 	}else(assn_operator == 3){
-		operator1=*;
+		operator1='*';
 	}
+
 	//User instructions
 	printf("Let's answer some randomly generated math questions. Simply enter the answer and press 'Enter'.\n");
 	
 	//Assemble the math question
-	printf("%d %d %d = ",value1,assn_operator,value2);
+	printf("%d %d %d = ",value1,operator1,value2);
 	scanf("%d\n", &answer);
 
 	//Check the right answer
-	if(answer == (value1
+	//if(answer == (value1
 	
-
+	return 0;
 
 }
