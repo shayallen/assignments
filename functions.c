c8bdf4ef1afd5cb2a9b5010237d9f99494760b35
Shay Allen <shayallen@gmail.com>
2013-03-18 15:23:56 -0600

    Working on incorporating the operator assignment portion into this function

--- functions.c
+++ functions.c
@@ -1,16 +1,42 @@
 #include<stdio.h>
+//variable declaration
+char operator;
+int x,y,result;
+float answer,real_answer,user_answer;
+
+//function for assigned variables
+float test_func(float x, float y){
+	float answer = x(operator)y;
+	return(answer);
+}
+//Operator assignment logic
+char assn_operator(char operator){
+	int get_operator = rand () % 4;
+        if(get_operator == 0){
+                operator = '+';
+                real_answer = x + y;
+        }else if(get_operator == 1){
+                operator = '-';
+                real_answer = x - y;
+        }else if(get_operator == 2){
+                operator = '/';
+                real_answer = x / y;
+                real_answer = floor(real_answer*100)/100;
+        }else{
+                operator = '*';
+                real_answer = x * y;
+	return (operator)
+        }
+
+	
+}
 int main(void){
-	int x, y, result;
 	//gather variable assignments
 	printf("Lets do some testing.  Enter two numbers when prompted.\n");
 	scanf("%i\n", &x);
-	scanf("%i\n", &y);
-	//function for assigned variables
-	int test_func(int x, int y){
-		int answer = x*y;
-		return(answer);
-	}
-	//run the function
+	scanf("%i", &y);
+	//run the functions
+	operator = assn_operator();
 	result = test_func(x,y);
 	printf("%i\n", result);
 	return 0;
